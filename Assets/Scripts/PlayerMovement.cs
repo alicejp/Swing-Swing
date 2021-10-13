cb748b1aeffbd9a5c300dfa41047307b84b72f98
Alicejp <alail5077@gmail.com>
2021-10-13 11:56:21 +0100

    Add jump movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
+    public float jumpOffSpeed = 4f;
+    public float minGroundNormalY = -3f;
     Rigidbody2D rb2D;
     float horizontalInput;
+    bool grounded;
 
     void Awake() 
     {
@@ -20,6 +23,13 @@ public class PlayerMovement : MonoBehaviour
         {
             var movement = new Vector2(horizontalInput, 0);
             rb2D.AddForce(movement * speed);
+        }
+
+        grounded = (this.transform.position.y > minGroundNormalY);
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            rb2D.velocity =  new Vector2(rb2D.velocity.x, jumpOffSpeed);
         }
     }
 }
